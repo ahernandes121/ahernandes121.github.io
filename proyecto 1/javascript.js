05b84f02bc0c5bac62f716180324518846a5629e
ahernandes121 <149205157+ahernandes121@users.noreply.github.com>
2023-10-31 11:50:57 -0600

    Add files via upload

---
+++ proyecto 1/javascript.js
@@ -0,0 +1,13 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2,'0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeStrig = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = timeStrig;
+}
+
+
+setInterval(updateClock, 1000);
+updateClock();
