2bd3f1a0577cc965b55cdb5a01ae7ae2499742d0
ahernandes121 <149205157+ahernandes121@users.noreply.github.com>
2023-10-31 12:49:06 -0600

    Add files via upload

---
+++ sumar dos numeros/javascript.js
@@ -0,0 +1,13 @@
+
+//Definicion de la suma(a, b)
+function sumar(a, b) {
+    return a + b;
+} 
+
+//Llamada a la funcion con los numeros 8 y 17
+const numero1 = 8;
+const numero2 = 17;
+const numero3 = sumar (numero1,numero2);
+
+//Mostrar el resultado en la consola del navegador
+console.log(`la suma de ${numero1} y ${numero2} es ${resultado}`);
